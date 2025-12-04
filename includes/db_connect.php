c3c130daff6d1a9f3c9eaa40938c879cccc53d35
Aline-eng <alinenzikwinkunda@gmail.com>
2025-12-04 17:31:02 +0200

    removed unnecessary comments

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -2,9 +2,9 @@
 // db_connect.php
 // Database connection parameters
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root'); // Default XAMPP username
-define('DB_PASSWORD', '');     // Default XAMPP password (empty)
-define('DB_NAME', 'online_medical_system'); // Name of your database
+define('DB_USERNAME', 'root'); 
+define('DB_PASSWORD', '');     
+define('DB_NAME', 'online_medical_system'); 
 
 // Attempt to connect to MySQL database
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
